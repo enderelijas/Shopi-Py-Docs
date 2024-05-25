e55ae6f3214ec4b21bb86dbbcfd9ebd2577c97c0
enderelijas <50271707+enderelijas@users.noreply.github.com>
2024-05-25 16:42:36 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Shopi-Py'
+copyright = '2024, enderelijas'
+author = 'enderelijas'
 
 release = '0.1'
 version = '0.1.0'
